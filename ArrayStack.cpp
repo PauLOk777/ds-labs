8c0c8a11f5127d7dfd92617c3b7f1c1bc2bb8a60
PauLOk777 <42682965+PauLOk777@users.noreply.github.com>
2019-02-16 22:36:45 +0200

    Lab-1(ArrayStack)_4_var

---
+++ ArrayStack.cpp
@@ -0,0 +1,73 @@
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+class MyStack {
+private:
+	char *stack;
+	int Size;
+	int last;
+public:
+	MyStack(int valueSize) {
+		Size = valueSize;
+		this->stack = new char[Size];
+		last = 0;
+	}
+
+	void push(char value) {
+		if (last == Size) {
+			cout << "Your stack is full!!!\n";
+			exit(1);
+		}
+		stack[last] = value;
+		last++;
+	}
+	bool pop() {
+		if (last < 1) {
+			cout << "Stack is empty!!!\n";
+			return false;
+		}
+		else {
+			int value;
+			value = stack[last];
+			stack[last] = NULL;
+			last--;
+			cout << "You deleted (" << value << ")\n";
+			return true;
+		}
+
+	}
+
+	char top() {
+		if (last == 0) {
+			cout << "Stack is empty!\n";
+		}
+		else {
+			return stack[last];
+		}
+	}
+
+	void Clear() {
+		if (last == 0) {
+			cout << "Well done. Stack is empty!\n";
+		}
+		else {
+			stack[last] = NULL;
+			last--;
+		}
+	}
+
+	int Size() {
+		return last;
+	}
+
+	bool isEmpty() {
+		bool result = (last == 0);
+		return result;
+	}
+
+	~MyStack() {
+		delete[] stack;
+  }
+};
