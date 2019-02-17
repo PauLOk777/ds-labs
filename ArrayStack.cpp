021e50fd3673dfaf8823be15df9d09edb726a69e
PauLOk777 <42682965+PauLOk777@users.noreply.github.com>
2019-02-17 13:28:20 +0200

    Update ArrayStack.cpp

--- ArrayStack.cpp
+++ ArrayStack.cpp
@@ -4,20 +4,21 @@ using namespace std;
 
 class MyStack {
 private:
-	char *stack;
-	int Size;
-	int last;
+	char *stack;     //Указатель на наш стек.
+	int Size;		//Размер стека.
+	int last;		//Количество логических элементов стека.
 public:
 	MyStack(int valueSize) {
 		Size = valueSize;
-		this->stack = new char[Size];
+		this->stack = new char[Size];				//Конструктор нашего стека.
 		last = 0;
+		stack[last] = NULL;
 	}
 
-	void push(char value) {
+	void push(char value) {					
 		if (last == Size) {
 			cout << "Your stack is full!!!\n";
-			exit(1);
+			exit(1);								//Добавляем в конец стека елемент.
 		}
 		stack[last] = value;
 		last++;
@@ -30,8 +31,8 @@ public:
 			return false;
 		}
 		else {
-			char value;
-			value = stack[last - 1];
+			char value;								//Удаляем последний елемент и выводим его на экран.
+			value = stack[last - 1];					
 			stack[last-1] = NULL;
 			last--;
 			cout << "You deleted (" << value << ")\n";
@@ -43,7 +44,7 @@ public:
 	char top() {
 		if (last == 0) {
 			cout << "Stack is empty!\n";
-		}
+		}											//Выводим на экран последний элемент без его удаления.
 		else {
 			return stack[last - 1];
 		}
@@ -54,20 +55,20 @@ public:
 			cout << "Well done. Stack is empty!\n";
 		}
 		else {
-			for (; last!=0; last--) {
+			for (; last!=0; last--) {					//Полная очистка стека.
 				stack[last - 1] = NULL;
 			}
 		}
 	}
 
 	int size() {
-		return last;
+		return last;									//Возвращаем размер стека.
 	}
 
 	bool isEmpty() {
 		if (last == 0) {
 			return true;
-		}
+		}												//Проверка на пустоту стека.
 		else {
 			return false;
 		}
@@ -86,7 +87,7 @@ public:
 			}
 			newStack[i] = temp;
 			for (; i < last; i++) {
-				newStack[i + 1] = stack[last - i - 1];
+				newStack[i + 1] = stack[last - i - 1];		//Добавления в середину стека элемента.
 				stack[last - i - 1] = NULL;
 			}
 			last++;
@@ -101,18 +102,28 @@ public:
 	}
 
 	~MyStack() {
-		delete[] stack;
+		delete[] stack;				//Деструктор нашего стека.
 	}
 
-	void Print() {
+	void Show() {
 		if (last == 0) {
 			cout << "Stack is empty!\n";
 		}
 		else {
+			char *newStack = new char[Size];
 			for (int i = 0; i < last; i++) {
-				cout << stack[i] << "  ";
+				newStack[i] = stack[last - i - 1];
+				stack[last - i - 1] = NULL;					//Вывод нашего стека на экран без его удаления.
+			}
+			newStack[last] = NULL;
+			for (int i = 0; i < last; i++) {
+				stack[i] = newStack[last - i - 1];
+				cout << newStack[last - i - 1] << " ";
+				newStack[last - i - 1] = NULL;
 			}
 			cout << endl;
+			stack[last] = NULL;
+			delete[] newStack;
 		}
 	}
 };
@@ -124,26 +135,31 @@ int main() {
 	MyStack Stack(size);
 	char ch;
 	cout << "Input your char: "; cin >> ch;
+	
 	Stack.push(ch);
 	Stack.push(ch);
 	Stack.push(ch);
 	Stack.push(ch);
 
-	Stack.Print();
+	Stack.Show();
 
-	Stack.PushMiddle('&');
+	char MyCh;
+	cout << "Input which element you wanna to add: "; cin >> MyCh;
 
-	Stack.Print();
+	Stack.PushMiddle(MyCh);
+
+	Stack.Show();
 
 	Stack.pop();
 
-	Stack.Print();
+	Stack.Show();
 
 	cout << "Size of stack: " << Stack.size() << endl;
 	
 	cout << "Head of stack: " << Stack.top() << endl;
 
 	Stack.clear();
+	
 	if (Stack.isEmpty()) {
 		cout << "Stack is really empty.\n";
 	}
