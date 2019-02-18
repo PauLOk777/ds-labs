b01b48a61d7834e93d40ef830de6beda26682f3e
PauLOk777 <42682965+PauLOk777@users.noreply.github.com>
2019-02-18 19:56:28 +0200

    Update LinkedStack.cpp

--- LinkedStack.cpp
+++ LinkedStack.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 struct elem {
-	char data;
-	elem *next;
+	char data;			//Даные элемента.
+	elem *next;			//Ссылка на предыдущий элемент.
 };
 
 struct MyStack {
-	elem *head;
-	int size;
+	elem *head;			//Ссылка на начало стека(верхний элемент).
+	int size;			//Размер стека.
 	MyStack() {
 		head = NULL;
 		size = 0;
@@ -19,85 +19,127 @@ struct MyStack {
 void push(MyStack &other, char value) {
 	elem *temp = new elem;
 	temp->data = value;
-	temp->next = other.head;
+	temp->next = other.head;					//Добавление элемента в стэк.
 	other.head = temp;
 	other.size++;
 }
 
-bool pop(MyStack &other, char &value) {
+void pop(MyStack &other, char &value) {
 	if (!other.head) {
 		cout << "Stack is empty!\n";
-		return false;
+		return;
 	}
-
 	elem *del = other.head;
-	value = other.head->data;
+	value = other.head->data;					//Удаление елемента из стэка.
 	other.head = other.head->next;
 	delete del;
 	other.size--;
-	return true;
+	return;
 }
 
 bool isEmpty(MyStack &other) {
-	return (other.size == 0);
+	return (other.size == 0);					//Проверка на пустоту стека.
 }
 
 char top(MyStack &other) {
-	elem *temp = other.head;
-	return temp->data;
+	if (other.size == 0) {
+		cout << "Stack is empty!\n";
+	}											//Возврат крайнего єлемента стека.
+	else {
+		return other.head->data;
+	}
 }
 
 void clear(MyStack &other) {
-	elem *del;
-	while (other.head) {
-		del = other.head;
-		other.head = other.head->next;
-		delete other.head;
+	char temp;
+	for (int i = 0; other.size != 0; i++) {
+		pop(other, temp);							//Очищение стека.
+		cout << "Deleted: " << temp << endl;
 	}
 	cout << "Your stack is clear!\n";
 }
 
-void PushMiddle(MyStack &other, char value) {
+void PushMiddle(MyStack &other,const char value) {
 	if (other.size == 0) {
 		cout << "Stack is empty!\n";
 		return;
 	}
+	char check;
 	MyStack newStack;
-	for (int i = 0; i < other.size/2; i++) {
-		
+	int newSize = other.size;
+	for (int i = 0; i < newSize/2; i++) {					//Добавление элемента в середину, если кол-во
+		pop(other, check);						//элементов парное и выше середины, если непарное.
+		push(newStack, check);
+	}
+	push(other, value);
+	for (int i = 0; i < newSize/2; i++) {
+		pop(newStack, check);
+		push(other, check);
 	}
 }
 
 void Show(MyStack &other) {
-	elem *temp = other.head;
-	while (temp != NULL) {
-		cout << temp->data << " ";
-		temp = temp->next;
+	if (other.size == 0) {
+		cout << "Stack is empty!\n";
 	}
-	cout << endl;
+	else {
+		char value;
+		int newSize = other.size;
+		MyStack newStack;
+		for (int i = 0; i < newSize; i++) {
+			pop(other, value);						//Вывод нашего стека на экран без его удаления.
+			push(newStack, value);
+		}
+		for (int i = 0; i < newSize; i++) {
+			pop(newStack, value);
+			cout << value << " ";
+			push(other, value);
+		}
+		cout << endl;
+	}
 }
 
 int size(const MyStack &other) {
-	return other.size;
+	return other.size;							//Возврат размера стека.
 }
 
 int main() {
 	MyStack x;
-	push(x, '5');
-	push(x, '7');
-	push(x, '3');
+	char ch;
+	cout << "We will add your symbol to stack 5th times.\n";
+	cout << "Input your char: "; cin >> ch;
+	push(x, ch);
+	push(x, ch);
+	push(x, ch);
+	push(x, ch);
+	push(x, ch);
+
 	Show(x);
 
-	char value = '0';
-	if (pop(x, value))
-		cout << value << endl;
+	char MyCh;
+	cout << "Input which element you wanna add: "; cin >> MyCh;
+	PushMiddle(x, MyCh);
 	Show(x);
-	if (pop(x, value))
-		cout << value << endl;
-	cout << top(x) << endl;
+
+	cout << "Now we will delete the head of stack\n";
+	char temp;
+	pop(x, temp);
+	cout << "We deleted: " << temp << endl;
+	Show(x);
+
+	cout << "The head of stack: " << top(x) << endl;
+
+	cout << "Size of stack: " << size(x) << endl;
+
+	cout << "Now we will clear all stack.\n";
 	clear(x);
-	if (pop(x, value))
-		cout << value << endl;
+
+	if (isEmpty(x)) {
+		cout << "Really clear!\n";
+	}
+	else {
+		cout << "He is not clear.\n";
+	}
 
 	return 0;
 }
