4835bcfc04434f62f48ff81e1c1a6880b7a50092
PauLOk777 <42682965+PauLOk777@users.noreply.github.com>
2019-04-27 16:10:07 +0300

    Update Lab-7(Graph).cpp

--- Lab-7(Graph).cpp
+++ Lab-7(Graph).cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <vector>
 #include <ctime>
+#include <list>
 
 using namespace std;
 
@@ -15,6 +16,7 @@ public:
 	~MySquadMatrix();
 	void resize(int);
 	int** getMatrix() { return squadMatrix; }
+	int getSize() { return size; }
 };
 
 MySquadMatrix::MySquadMatrix() {
@@ -86,6 +88,8 @@ public:
 	void randomGraph(int);
 	void labGraph();
 	void showGraph();
+	void searchInWidth();
+	void searchInDepth();
 };
 
 Graph::Graph()
@@ -136,7 +140,7 @@ void Graph::randomGraph(int size)
 				count++;
 				continue;
 			}
-			if (count % 2 == 0) {
+			if (count % 3 == 0) {
 				adjacencyMatrix.getMatrix()[i][j] = adjacencyMatrix.getMatrix()[j][i] = rand() % 30 + 1;
 				count++;
 				continue;
@@ -152,7 +156,33 @@ void Graph::randomGraph(int size)
 
 void Graph::labGraph()
 {
-
+	int staticMatrix[17][17] = {
+	//	 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16
+		{0, 4, 7, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//0
+		{4, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//1
+		{7, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0},//2
+		{3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0},//3
+		{0, 2, 2, 0, 0, 0, 4, 0, 0, 0, 7, 0, 0, 8, 0, 8, 0},//4
+		{0, 0, 0, 0, 0, 0, 0, 7, 3, 0, 0, 0, 1, 9,11, 0, 0},//5
+		{0, 0, 0, 0, 4, 0, 0, 0, 4, 0, 6, 0, 0, 0, 0, 0, 0},//6
+		{0, 0, 0, 0, 0, 7, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0},//7
+		{0, 0, 0, 0, 0, 3, 4, 0, 0, 3, 0, 0, 9, 0, 0,15, 0},//8
+		{0, 0, 0, 0, 0, 0, 0, 5, 3, 0, 0, 0, 2, 0, 3, 0,12},//9
+		{0, 0, 2, 7, 7, 0, 6, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0},//10
+		{0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 4, 0, 0, 0, 0, 0, 0},//11
+		{0, 0, 0, 0, 0, 1, 0, 0, 9, 2, 0, 0, 0, 0, 0, 0,11},//12
+		{0, 0, 0, 0, 8, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},//13
+		{0, 0, 0, 0, 0,11, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 8},//14
+		{0, 0, 0, 0, 8, 0, 0, 0,15, 0, 0, 0, 0, 1, 0, 0, 0},//15
+		{0, 0, 0, 0, 0, 0, 0, 0, 0,12, 0, 0,11, 0, 8, 0, 0},//16
+	};
+	adjacencyMatrix.resize(17);
+	vertices = adjacencyMatrix.getSize();
+	for (int i = 0; i < vertices; i++) {
+		for (int j = 0; j < vertices; j++) {
+			adjacencyMatrix.getMatrix()[i][j] = staticMatrix[i][j];
+		}
+	}
 }
 
 void Graph::showGraph()
@@ -172,12 +202,67 @@ void Graph::showGraph()
 	}
 }
 
+void Graph::searchInWidth()
+{
+	list<int> passedVertices;
+	list<int> tempList_1;
+	list<int> tempList_2;
+	int count = 0;
+	for (int i = 0; count < vertices;) {
+		if (!i) tempList_2.push_back(i);
+		for (int j = 0; j < vertices; j++) {
+			if (adjacencyMatrix.getMatrix()[i][j] != 0) {
+				tempList_1.push_back(j);
+			}
+		}
+		for (auto it = tempList_1.begin(); it != tempList_1.end(); it++) {
+			bool flag_3 = true;
+			for (auto it2 = tempList_2.begin(); it2 != tempList_2.end(); it2++) {
+				if (*it == *it2) flag_3 = false;
+			}
+			if (flag_3) tempList_2.push_back(*it);
+		}
+		for (auto it = tempList_2.begin(); it != tempList_2.end(); it++) {
+			bool flag_2 = true;
+			for (auto it2 = passedVertices.begin(); it2 != passedVertices.end(); it2++) {
+				if(*it == *it2) flag_2 = false;
+			}
+			if(flag_2) passedVertices.push_back(*it);
+		}
+		auto iterator = passedVertices.begin();
+		advance(iterator, count);
+		i = *iterator;
+		count++;
+		tempList_1.clear();
+	}
+	cout << "Search in width: " << endl;
+	cout << "Graph from which you start bypass: " << endl;
+	for (auto i = passedVertices.begin(); i != passedVertices.end(); ++i) {
+		cout << *i << " ";
+	}
+	cout << endl;
+	cout << "Another vertices(not in your graph): " << endl;
+	for (int i = 0; i < vertices; i++) {
+		bool flag = true;
+		for (auto it = passedVertices.begin(); it != passedVertices.end(); ++it) {
+			if (i == *it) flag = false;
+		}
+		if (flag) cout << i << " ";
+	}
+	cout << endl;
+}
+
+void Graph::searchInDepth()
+{
+
+}
+
 int main() {
 	int num, temp;
-	cout << "How many vertex do you wanna add: "; cin >> num;
 	Graph graph;
-	cout << "If you wanna fill manually press 1, if no - any number: "; cin >> temp;
+	cout << "If you wanna fill manually press 1, if random - 2, graph from lab - 3: "; cin >> temp;
 	if (temp == 1) {
+		cout << "How many vertex do you wanna add: "; cin >> num;
 		for (int i = 0; i < num; i++) {
 			int num_2;
 			cout << "How many adjacent vertices has " << i << " vertex: "; cin >> num_2;
@@ -196,13 +281,24 @@ int main() {
 				cin >> ArrayOfWeights[j];
 			}
 			graph.insert(ArrayOfVertices, ArrayOfWeights);
+			delete[] ArrayOfVertices;
+			delete[] ArrayOfWeights;
 		}
 		graph.showGraph();
+		graph.searchInWidth();
 	}
-	else {
+	if(temp == 2) {
+		cout << "How many vertex do you wanna add: "; cin >> num;
 		graph.randomGraph(num);
 		graph.showGraph();
+		graph.searchInWidth();
 	}
+	if (temp == 3) {
+		graph.labGraph();
+		graph.showGraph();
+		graph.searchInWidth();
+	}
+
 	system("pause");
 	return 0;
 }
