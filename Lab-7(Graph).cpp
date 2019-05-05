416e295bd398b434c7609aa02147fbf625fdef01
PauLOk777 <42682965+PauLOk777@users.noreply.github.com>
2019-05-05 22:31:46 +0300

    Update Lab-7(Graph).cpp

--- Lab-7(Graph).cpp
+++ Lab-7(Graph).cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <ctime>
 #include <list>
+#include <string>
 
 using namespace std;
 
@@ -129,11 +130,14 @@ public:
 	void showGraph();
 	void showGraphWeight();
 	void showGraphRoad();
-	void searchInWidth();
-	void searchInDepth();
+	void searchInWidth(int);
+	void searchInDepth(int);
 	int DijkstraAlgorithm(int, int);
 	int sumWeigth();
 	void FloydAlgoithm();
+	void BellmanFordAlgorithm(int, int*, int*);
+	void FillWeightMatrix();
+	void FillRoadMatrix();
 };
 
 Graph::Graph()
@@ -184,10 +188,9 @@ void Graph::randomGraph(int size)
 		for (int j = 0; j < i + 1; j++) {
 			if (j == i) {
 				adjacencyMatrix.getMatrix()[i][j] = 0;
-				count++;
 				continue;
 			}
-			if (count % 3 == 0) {
+			if (count % 2 == 0) {
 				adjacencyMatrix.getMatrix()[i][j] = adjacencyMatrix.getMatrix()[j][i] = rand() % 30 + 1;
 				count++;
 				continue;
@@ -260,7 +263,12 @@ void Graph::showGraphWeight()
 	for (int i = 0; i < vertices; i++) {
 		cout << i << "	";
 		for (int j = 0; j < vertices; j++) {
-			cout << setw(5) << left << weightMatrix.getMatrix()[i][j];
+			if (weightMatrix.getMatrix()[i][j] >= sumWeigth() * 2) {
+				cout << setw(5) << left << "no";
+			}
+			else {
+				cout << setw(5) << left << weightMatrix.getMatrix()[i][j];
+			}
 		}
 		cout << endl;
 	}
@@ -278,19 +286,19 @@ void Graph::showGraphRoad()
 		cout << i << "	";
 		for (int j = 0; j < vertices; j++) {
 			cout << setw(5) << left << roadMatrix.getMatrix()[i][j];
+			
 		}
 		cout << endl;
 	}
 }
 
-void Graph::searchInWidth()
+void Graph::searchInWidth(int begin)
 {
 	list<int> tempList_1;
 	list<int> passedVertices;
-	int count = 0, temp;
-	cout << "Input from which element you will start: "; cin >> temp;
-	passedVertices.push_back(temp);
-	for (int i = temp; count < vertices; count++) {
+	int count = 0;
+	passedVertices.push_back(begin);
+	for (int i = begin; count < vertices; count++) {
 		for (int j = 0; j < vertices; j++) {
 			if (adjacencyMatrix.getMatrix()[i][j] != 0) {
 				tempList_1.push_back(j);
@@ -326,13 +334,12 @@ void Graph::searchInWidth()
 	cout << endl;
 }
 
-void Graph::searchInDepth()
+void Graph::searchInDepth(int begin)
 {
 	list<int> passedVertices;
-	int temp, count_1 = 0;
-	cout << "Input element from which you will start: "; cin >> temp;
-	passedVertices.push_back(temp);
-	for (int i = temp; i < vertices;) {
+	int count_1 = 0;
+	passedVertices.push_back(begin);
+	for (int i = begin; i < vertices;) {
 		int count = 0;
 		for (int j = 0; j < vertices; j++) {
 			if (adjacencyMatrix.getMatrix()[i][j] != 0) {
@@ -431,28 +438,8 @@ int Graph::sumWeigth()
 
 void Graph::FloydAlgoithm()
 {
-	weightMatrix.resize(vertices);
-	roadMatrix.resize(vertices);
-	for (int i = 0; i < vertices; i++) {
-		for (int j = 0; j < vertices; j++) {
-			if (adjacencyMatrix.getMatrix()[i][j] == 0 && i != j) {
-				weightMatrix.getMatrix()[i][j] = this->sumWeigth() * 2;
-			}
-			else {
-				weightMatrix.getMatrix()[i][j] = adjacencyMatrix.getMatrix()[i][j];
-			}
-			if (adjacencyMatrix.getMatrix()[i][j] != 0) {
-				roadMatrix.getMatrix()[i][j] = j;
-			}
-			else {
-				roadMatrix.getMatrix()[i][j] = -1;
-			}
-			if (i == j) {
-				roadMatrix.getMatrix()[i][j] = i;
-				weightMatrix.getMatrix()[i][j] = 0;
-			}
-		}
-	}
+	FillWeightMatrix();
+	FillRoadMatrix();
 
 	for (int i = 0; i < vertices; i++) {
 		for (int j = 0; j < vertices; j++) {
@@ -479,15 +466,194 @@ void Graph::FloydAlgoithm()
 	showGraphRoad();
 }
 
-void DijkstraForAll(Graph & graph) {
-	for (int i = 0; i < graph.getVertices(); i++) {
-		for (int j = 0; j < graph.getVertices(); j++) {
-			if (graph.sumWeigth() * 2 == graph.DijkstraAlgorithm(i, j)) {
-				cout << "From " << i << " to " << j << " no road." << endl;
+void Graph::BellmanFordAlgorithm(int elem, int *RoadArray, int *WeightArray)
+{
+	for (int i = 0; i < vertices; i++) {
+		RoadArray[i] = -1;
+		WeightArray[i] = this->sumWeigth() * 2;
+	}
+	RoadArray[elem] = 0;
+	WeightArray[elem] = 0;
+	list<int> forPush;
+	list<int> uniqueNums;
+	uniqueNums.push_back(elem);
+	int count = 0;
+	for (int i = elem;;) {
+		for (int j = 0; j < vertices; j++) {
+			if (adjacencyMatrix.getMatrix()[i][j] != 0) {
+				forPush.push_back(j);
+				bool flag = true;
+				for (auto it = uniqueNums.begin(); it != uniqueNums.end(); it++) {
+					if (*it == j) flag = false;
+				}
+				if (flag) uniqueNums.push_back(j);
+				if (WeightArray[j] > WeightArray[i] + adjacencyMatrix.getMatrix()[i][j]) {
+					WeightArray[j] = WeightArray[i] + adjacencyMatrix.getMatrix()[i][j];
+					RoadArray[j] = i;
+				}
+			}
+		}
+		if (count + 1 >= vertices || uniqueNums.size() < count + 1) break;
+		auto iterator = uniqueNums.begin();
+		advance(iterator, count);
+		i = *iterator;
+		forPush.clear();
+		count++;
+	}
+}
+
+void Graph::FillWeightMatrix()
+{
+	weightMatrix.resize(vertices);
+	for (int i = 0; i < vertices; i++) {
+		for (int j = 0; j < vertices; j++) {
+			if (adjacencyMatrix.getMatrix()[i][j] == 0 && i != j) {
+				weightMatrix.getMatrix()[i][j] = this->sumWeigth() * 2;
 			}
 			else {
-				cout << "From " << i << " to " << j << " = " << graph.DijkstraAlgorithm(i, j) << endl;
+				weightMatrix.getMatrix()[i][j] = adjacencyMatrix.getMatrix()[i][j];
 			}
+			if (i == j) {
+				weightMatrix.getMatrix()[i][j] = 0;
+			}
+		}
+	}
+}
+
+void Graph::FillRoadMatrix()
+{
+	roadMatrix.resize(vertices);
+	for (int i = 0; i < vertices; i++) {
+		for (int j = 0; j < vertices; j++) {
+			if (adjacencyMatrix.getMatrix()[i][j] != 0) {
+				roadMatrix.getMatrix()[i][j] = j;
+			}
+			else {
+				roadMatrix.getMatrix()[i][j] = -1;
+			}
+			if (i == j) {
+				roadMatrix.getMatrix()[i][j] = i;
+			}
+		}
+	}
+}
+
+void DijkstraForAll(Graph & graph) {
+	MySquadMatrix newMatrix;
+	newMatrix.resize(graph.getVertices());
+	cout << "Dijkstra algorithm: " << endl;
+	for (int i = 0; i < graph.getVertices(); i++) {
+		for (int j = i; j < graph.getVertices(); j++) {
+			if (i == j) newMatrix.getMatrix()[i][j] = 0;
+			else {
+				newMatrix.getMatrix()[i][j] = newMatrix.getMatrix()[j][i] = graph.DijkstraAlgorithm(i, j);
+			}
+		}
+	}
+	for (int i = 0; i < graph.getVertices(); i++) {
+		if (!i) cout << "	";
+		cout << setw(5) << left << i;
+	}
+	cout << endl << endl;
+	for (int i = 0; i < graph.getVertices(); i++) {
+		cout << i << "	";
+		for (int j = 0; j < graph.getVertices(); j++) {
+			if (graph.sumWeigth() * 2 == newMatrix.getMatrix()[i][j]) {
+				cout << setw(5) << left << "no";
+			}
+			else {
+				cout << setw(5) << left << newMatrix.getMatrix()[i][j];
+			}
+		}
+		cout << endl;
+	}
+}
+
+void BellmanFordForAll(Graph & graph) {
+	int* RoadArray = new int[graph.getVertices()];
+	int* WeightArray = new int[graph.getVertices()];
+	for (int i = 0; i < graph.getVertices(); i++) {
+		cout << "Bellman-Ford for " << i << " vertex:\n";
+		graph.BellmanFordAlgorithm(i, RoadArray, WeightArray);
+		for (int j = 0; j < graph.getVertices(); j++) {
+			if (!j) cout << "    ";
+			cout << setw(4) << left << j;
+		}
+		cout << endl << "St: ";
+		for (int j = 0; j < graph.getVertices(); j++) {
+			cout << setw(4) << left << RoadArray[j];
+		}
+		cout << endl << "Wt: ";
+		for (int j = 0; j < graph.getVertices(); j++) {
+			if (graph.sumWeigth() * 2 <= WeightArray[j]) {
+				cout << setw(4) << left << "no";
+			}
+			else {
+				cout << setw(4) << left << WeightArray[j];
+			}
+		}
+		cout << endl;
+	}
+	delete[] RoadArray;
+	delete[] WeightArray;
+}
+
+void insert(Graph& graph) {
+	int num;
+	cout << "How many vertex do you wanna add: "; cin >> num;
+	for (int i = 0; i < num; i++) {
+		int num_2;
+		cout << "How many adjacent vertices has " << i << " vertex: "; cin >> num_2;
+		if (!num_2) {
+			graph.insert();
+			continue;
+		}
+		int* ArrayOfVertices = new int[num_2];
+		int* ArrayOfWeights = new int[num_2];
+		cout << "Enter the vertices with which it is adjacent: ";
+		for (int j = 0; j < num_2; j++) {
+			cin >> ArrayOfVertices[j];
+		}
+		cout << "Enter weights of this edges: ";
+		for (int j = 0; j < num_2; j++) {
+			cin >> ArrayOfWeights[j];
+		}
+		graph.insert(ArrayOfVertices, ArrayOfWeights, num_2);
+		delete[] ArrayOfVertices;
+		delete[] ArrayOfWeights;
+	}
+}
+
+void bypass(Graph &graph) {
+	string num;
+	while (true) {
+		cout << "Input from which element you wanna start width bypass(if you wanna exit type 'quit'): ";
+		cin >> num;
+		if (num == "quit") break;
+		graph.searchInWidth(stoi(num));
+	}
+	while (true) {
+		cout << "Input from which element you wanna start depth bypass(if you wanna exit type 'quit'): ";
+		cin >> num;
+		if (num == "quit") break;
+		graph.searchInDepth(stoi(num));
+	}
+}
+
+void algorithms(Graph& graph) {
+	string num;
+	while (true) {
+		cout << "If you wanna use Dijkstra press 1, Floyd - 2, Bellman-Ford - 3, for exit type 'quit': ";
+		cin >> num;
+		if (num == "quit") break;
+		if (stoi(num) == 1) {
+			DijkstraForAll(graph);
+		}
+		if (stoi(num) == 2) {
+			graph.FloydAlgoithm();
+		}
+		if (stoi(num) == 3) {
+			BellmanFordForAll(graph);
 		}
 	}
 }
@@ -497,49 +663,23 @@ int main() {
 	Graph graph;
 	cout << "If you wanna fill manually press 1, if random - 2, graph from lab - 3: "; cin >> temp;
 	if (temp == 1) {
-		cout << "How many vertex do you wanna add: "; cin >> num;
-		for (int i = 0; i < num; i++) {
-			int num_2;
-			cout << "How many adjacent vertices has " << i << " vertex: "; cin >> num_2;
-			if (!num_2) {
-				graph.insert();
-				continue;
-			}
-			int* ArrayOfVertices = new int[num_2];
-			int* ArrayOfWeights = new int[num_2];
-			cout << "Enter the vertices with which it is adjacent: ";
-			for (int j = 0; j < num_2; j++) {
-				cin >> ArrayOfVertices[j];
-			}
-			cout << "Enter weights of this edges: ";
-			for (int j = 0; j < num_2; j++) {
-				cin >> ArrayOfWeights[j];
-			}
-			graph.insert(ArrayOfVertices, ArrayOfWeights, num_2);
-			delete[] ArrayOfVertices;
-			delete[] ArrayOfWeights;
-		}
-		graph.showGraph();
-		graph.searchInWidth();
-		graph.searchInDepth();
-		DijkstraForAll(graph);
+		insert(graph);
+		bypass(graph);
+		algorithms(graph);
 	}
 	if (temp == 2) {
 		cout << "How many vertex do you wanna add: "; cin >> num;
 		graph.randomGraph(num);
 		graph.showGraph();
-		graph.searchInWidth();
-		graph.searchInDepth();
-		DijkstraForAll(graph);
+		bypass(graph);
+		algorithms(graph);
 	}
 	if (temp == 3) {
 		graph.labGraph();
 		graph.showGraph();
-		graph.searchInWidth();
-		graph.searchInDepth();
-		graph.FloydAlgoithm();
+		bypass(graph);
+		algorithms(graph);
 	}
-
 	system("pause");
 	return 0;
 }
